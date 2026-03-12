c232a94ba624fd85e0a6713377fc575e3ce6070b
Ann-Christin Alvaro <acm@ancosys.local>
2026-03-12 02:09:39 +0100

    update index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`✅ ${client.user.tag} ist online!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
